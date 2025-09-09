dafe1ace45ecb5af097fbca17d6d4c07054d5593
pratiwiindahkhofifah24-tech <pratiwiindahkhofifah24@gmail.com>
2025-09-09 20:26:04 +0800

    Update day002.java

--- day002.java
+++ day002.java
@@ -1,12 +1,11 @@
 public class Codingday2 {
 public static void main(String[] args) {
-System.out.print("Halo Fifaaa ");             
-System.out.print("19");          
+System.out.print("Halo Fifaaa ");
+System.out.print("19");
 
 System.out.println("Fifa");
 System.out.println("19");
-        
-System.out.printf("nama:%s,umur:%d, "fifaaa", 19);
+
+System.out.printf("nama:%s, umur:%d\n", "fifaaa", 19);
     }
 }
-
