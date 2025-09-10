7e3291305f4b9d117ddb561fc0314786d4b17d59
pratiwiindahkhofifah24-tech <pratiwiindahkhofifah24@gmail.com>
2025-09-10 20:01:36 +0800

    Update day003.java

--- day003.java
+++ day003.java
@@ -1,5 +1,11 @@
 public class Codingday3 {
 public static void main(String[] args) {
+  /* 
+ Program ini menyapa pengguna dengan menampilkan
+pesan "Halo Guys" ke layar, 
+variabel nama dengan nilai "fifaa" dan menampilkan nilainya.
+*/
+        
 // Ini adalah sapaan dari fifaa:
 System.out.println("Halo Guys");
 
