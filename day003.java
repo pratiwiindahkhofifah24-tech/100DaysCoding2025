d549c7a4ceaa2de012a6f6d9a14707da959aa545
pratiwiindahkhofifah24-tech <pratiwiindahkhofifah24@gmail.com>
2025-09-10 18:49:18 +0800

    Create day003.java

---
+++ day003.java
@@ -0,0 +1,10 @@
+public class Codingday3 {
+public static void main(String[] args) {
+// Ini adalah sapaan dari fifaa:
+System.out.println("Halo Guys");
+
+String nama = "fifaa";  
+// Variabel nama berisi "fifaa"
+System.out.println("Nama saya adalah " + nama);
+    }
+}
