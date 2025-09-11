3f2c634c29fb6fe4f48cd765f3aba81e314c548f
pratiwiindahkhofifah24-tech <pratiwiindahkhofifah24@gmail.com>
2025-09-11 22:25:18 +0800

    Create day004.java

---
+++ day004.java
@@ -0,0 +1,11 @@
+public class Codingday4 {
+public static void main(String[] args) {
+
+//mau cari ide lain tapi eror kak,jadi ini ajaa wkwk
+System.out.println("Halo \nGuys");
+System.out.println("Nama \t:Fifaa");
+System.out.println("Fakultas\tTeknik");
+System.out.println("Mereke bilang:\"harus kuat ngodingnyaa!\"");
+    }
+}
+
