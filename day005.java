2b73e7e7da2902ba9aeb57bcf77ff0fd800f0b17
pratiwiindahkhofifah24-tech <pratiwiindahkhofifah24@gmail.com>
2025-09-12 20:50:40 +0800

    Create day005.java

---
+++ day005.java
@@ -0,0 +1,12 @@
+public class Codingday5 {
+public static void main(String[] args) {
+        byte umur = 19;
+        short tahun = 2025;
+        int beratBadan = 57;
+        long tinggiBadan = 155;
+        System.out.println(umur);
+        System.out.println(tahun);
+        System.out.println(beratBadan);
+        System.out.println(tinggiBadan);
+    }
+}
