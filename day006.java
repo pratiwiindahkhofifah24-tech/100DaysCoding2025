21ebaac38e93268197d074dfc28c11e315202541
pratiwiindahkhofifah24-tech <pratiwiindahkhofifah24@gmail.com>
2025-09-13 22:23:23 +0800

    Create day006.java

---
+++ day006.java
@@ -0,0 +1,9 @@
+public class Codingday6 {
+public static void main(String[] args) {
+        float hargaEmas = 1.5f;
+        double gaji = 3.5;
+
+        System.out.println("Harga emas: " + hargaEmas);
+        System.out.println("Nilai gaji: " + gaji);
+    }
+}
