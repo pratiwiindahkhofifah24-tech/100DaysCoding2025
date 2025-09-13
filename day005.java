dda8007d1867e6f87755562a16efd4408d0795cc
pratiwiindahkhofifah24-tech <pratiwiindahkhofifah24@gmail.com>
2025-09-13 15:26:02 +0800

    Update day005.java

--- day005.java
+++ day005.java
@@ -4,9 +4,9 @@ public static void main(String[] args) {
         short tahun = 2025;
         int beratBadan = 57;
         long tinggiBadan = 155;
-        System.out.println(umur);
-        System.out.println(tahun);
-        System.out.println(beratBadan);
-        System.out.println(tinggiBadan);
+        System.out.println("umur");
+        System.out.println("tahun");
+        System.out.println("beratBadan");
+        System.out.println("tinggiBadan");
     }
 }
