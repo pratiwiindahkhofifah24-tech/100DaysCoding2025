0b1fe168b24a089970f977db3d51d7b76a3f7231
pratiwiindahkhofifah24-tech <pratiwiindahkhofifah24@gmail.com>
2025-09-14 20:26:56 +0800

    Create day007.java

---
+++ day007.java
@@ -0,0 +1,22 @@
+public class Codingday7 {
+public static void main(String[] args) {
+        // tipe data char
+        char inisial = 'K';
+        char bulan_lahir = '4';
+        char tipe_data = '8';
+
+        System.out.println("inisial: " + inisial);
+        System.out.println("bulan_lahir: " + bulan_lahir);
+        System.out.println("tipe_data: " + tipe_data);
+       
+       
+        //tipe data boolean
+        boolean olahraga = true;
+        boolean seni = false;
+        boolean belajar = true;
+        
+        System.out.println("olahraga:" + olahraga);
+        System.out.println("seni:" + seni);
+        System.out.println("belajar:" + belajar);
+    }
+}
