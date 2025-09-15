2dd0023656a76201640d25271e3464b5b6540d71
pratiwiindahkhofifah24-tech <pratiwiindahkhofifah24@gmail.com>
2025-09-15 23:04:51 +0800

    Create day008.java

---
+++ day008.java
@@ -0,0 +1,12 @@
+public class Codingday8 {
+public static void main(String[] args) {
+       
+        String salam = "Halo, selamat malam!";
+        String nama = "Fifaaaaaaaa";
+        String asal = "saya dari pambusuang";
+
+        System.out.println(salam);
+        System.out.println("Nama saya: " + nama);
+        System.out.println("asal: "+ asal);
+    }
+}
