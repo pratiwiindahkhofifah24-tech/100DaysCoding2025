c176e5cc73ae2973f192146e932c04b1157432a9
pratiwiindahkhofifah24-tech <pratiwiindahkhofifah24@gmail.com>
2025-09-16 21:23:07 +0800

    Create day009.java

---
+++ day009.java
@@ -0,0 +1,30 @@
+public class Codingday9 {
+    public static void main(String[] args) {
+        //Deklarasi
+        String Nama ;
+        int Umur ;
+        double berat_badan ;
+        char jenis_kelamin ;
+        
+        
+        //inisialisasi
+        Nama = "Fifaaa";
+        Umur = 19;
+        berat_badan = 53.5;
+        jenis_kelamin = 'p';
+        System.out.println(Nama);
+        System.out.println(Umur);
+        System.out.println(berat_badan);
+        System.out.println(jenis_kelamin);
+        
+        //update variable
+        Nama = "Fifaaa";
+        Umur = 20;
+        berat_badan = 55.5;
+        jenis_kelamin = 'p';
+        System.out.println("Nama:" + Nama);
+        System.out.println("Umur:" + Umur);
+        System.out.println("berat_badan:" + berat_badan);
+        System.out.println("jenis_kelamin:" + jenis_kelamin);
+        
+    }}
