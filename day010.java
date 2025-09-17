b52d4db8987a76e7fa6704626ffa15ac30c5e4cc
pratiwiindahkhofifah24-tech <pratiwiindahkhofifah24@gmail.com>
2025-09-17 23:27:33 +0800

    Update day010.java

--- day010.java
+++ day010.java
@@ -1,26 +1,9 @@
 public class Codingday10 {
     public static void main(String[] args) {
-        final String Nama = "Fifaaa";
-        final byte a = 1;
-        final short b = 2;
-        final char c = 'p';
-        final double d = 55.5;
-        final boolean e = true;
-        final boolean f = false;
-        final int g = 70;
-        final float h = 85.3f;
-        final long i = 100l;
-
-        System.out.println("ini variabel string " + Nama);
-        System.out.println("ini variabel byte " + a);
-        System.out.println("ini variabel short " + b);
-        System.out.println("ini variabel char " + c);
-        System.out.println("ini variabel double " + d);
-        System.out.println("ini variabel boolean " + e);
-        System.out.println("ini variabel boolean " + f);
-        System.out.println("ini variabel int " + g);
-        System.out.println("ini variabel float " + h);
-        System.out.println("ini variabel long " + i);
+        final String nama = "Fifaa";
+        final int JUMLAH_MAKS = 10;
+        
+        System.out.println("Nama:" + nama);
+        System.out.println("Jumlah maksimal: " + JUMLAH_MAKS);
     }
-
 }
