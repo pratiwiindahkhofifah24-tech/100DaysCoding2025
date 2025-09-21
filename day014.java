c3ba5f9e7909cdf807c75b75e8f51c5714127d58
pratiwiindahkhofifah24-tech <pratiwiindahkhofifah24@gmail.com>
2025-09-21 16:11:45 +0800

    Update day014.java

--- day014.java
+++ day014.java
@@ -1,11 +1,13 @@
 //mengubah string jadi tipe data primitif
 public class Codingday14 {
     public static void main(String[] args) {
-        int angka = Integer.parseInt("123");
-        double pecahan = Double.parseDouble("3.14");
-        char huruf = "A".charAt(0);
+        
+        String nt = "123";
+        int angka = Integer.parseInt(nt );
+        String ble = "3.14";
+        double pecahan = Double.parseDouble(ble);
 
         System.out.println("angka :" + angka);
         System.out.println("pecahan :" + pecahan);
-        System.out.println("huruf :" + huruf);
-    }}
+    }
+}
