60d05da1c2d0464d7140e5cd6498622fb5b1ff95
pratiwiindahkhofifah24-tech <pratiwiindahkhofifah24@gmail.com>
2025-09-21 11:11:07 +0800

    Create day014.java

---
+++ day014.java
@@ -0,0 +1,10 @@
+public class Codingday14 {
+    public static void main(String[] args) {
+        int angka = Integer.parseInt("123");
+        double pecahan = Double.parseDouble("3.14");
+        char huruf = "A".charAt(0);
+
+        System.out.println("angka :" + angka);
+        System.out.println("pecahan :" + pecahan);
+        System.out.println("huruf :" + huruf);
+    }}
