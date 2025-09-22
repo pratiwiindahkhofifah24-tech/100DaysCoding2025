105b699232935a98162c1f20cdfed8e8d202e87d
pratiwiindahkhofifah24-tech <pratiwiindahkhofifah24@gmail.com>
2025-09-22 22:06:04 +0800

    Create day015.java

---
+++ day015.java
@@ -0,0 +1,15 @@
+public class Codingday15 {
+public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        int penjumlahan = a + b;
+        int pengurangan = a - b;
+
+        System.out.println("Hasil penjumlahan: " + penjumlahan);
+        System.out.println("Hasil pengurangan: " + pengurangan);
+   
+   }}
+   
+   
+   
