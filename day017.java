87ec34166d8cb2c92fa70b543df4b9b3d5cd4569
pratiwiindahkhofifah24-tech <pratiwiindahkhofifah24@gmail.com>
2025-09-24 22:53:38 +0800

    Create day017.java

---
+++ day017.java
@@ -0,0 +1,12 @@
+public class Codingday17 {
+public static void main(String[] args) {
+
+	//operator aritmatika modulus atau sisa bagi(%)
+		int a = 9;
+    int b = 3;
+    int sisa = a % b;
+
+System.out.println("Sisa bagi dari " + a + "%" + b + " adalah " + sisa );
+
+	}
+}
