96dfa50ec6781c8b35f9ba71f21d976a710bec4a
pratiwiindahkhofifah24-tech <pratiwiindahkhofifah24@gmail.com>
2025-09-25 22:50:16 +0800

    Create day018.java

---
+++ day018.java
@@ -0,0 +1,10 @@
+public class Codingday18 {
+public static void main(String[] args) {
+        byte a1 = 10;
+        short a2 = a1;
+
+        System.out.println("byte : "+ a1);
+        System.out.println("short : "+ a2);
+
+    }
+}
