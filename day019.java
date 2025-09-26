a388d2a837c495a95fef1280de6d2c807fb47be8
pratiwiindahkhofifah24-tech <pratiwiindahkhofifah24@gmail.com>
2025-09-26 23:13:46 +0800

    Create day019.java

---
+++ day019.java
@@ -0,0 +1,11 @@
+//Konversi Manual/Paksa (Dari Tipe Data Besar ke Kecil).
+
+public class Codingday19 {
+public static void main(String[] args) {
+		int a1 = 100;
+		byte a2 = (byte) a1;
+		
+		System.out.println("hasil int a1 :" + a1);
+		System.out.println("hasil konversi :" + a2);
+	}
+}
