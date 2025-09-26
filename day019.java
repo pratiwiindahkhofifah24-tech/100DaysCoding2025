2b47d409cda1c6a4b31bff92cdcaa0d323d79bd6
pratiwiindahkhofifah24-tech <pratiwiindahkhofifah24@gmail.com>
2025-09-26 23:14:19 +0800

    Update day019.java

--- day019.java
+++ day019.java
@@ -2,10 +2,10 @@
 
 public class Codingday19 {
 public static void main(String[] args) {
-		int a1 = 100;
-		byte a2 = (byte) a1;
+int a1 = 100;
+byte a2 = (byte) a1;
 		
-		System.out.println("hasil int a1 :" + a1);
-		System.out.println("hasil konversi :" + a2);
+System.out.println("hasil int a1 :" + a1);
+System.out.println("hasil konversi :" + a2);
 	}
 }
