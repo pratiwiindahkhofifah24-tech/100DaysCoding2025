143becbae4a223e3bfd4c532b85539e89226e96d
pratiwiindahkhofifah24-tech <pratiwiindahkhofifah24@gmail.com>
2025-09-29 22:33:56 +0800

    Create day021.java

---
+++ day021.java
@@ -0,0 +1,14 @@
+public class Codingday21 {
+public static void main(String[] args) {
+	
+int a = 3;
+int b = 7;
+
+int temp = a;
+a = b;
+b = temp;
+
+System.out.println("a = " + a); 
+System.out.println("b = " + b); 
+	}
+}
