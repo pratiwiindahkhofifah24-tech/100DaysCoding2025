8ca0befa0ac49b0f75c0ce80b796187ac833d323
pratiwiindahkhofifah24-tech <pratiwiindahkhofifah24@gmail.com>
2025-10-02 22:24:50 +0800

    Create day025.java

---
+++ day025.java
@@ -0,0 +1,13 @@
+public class Codingday25 {
+    public static void main(String[] args) {
+        int x = 5;
+        
+        System.out.println("Awal x = " + x);
+        
+        x++;  // increment, x jadi 6
+        System.out.println("Setelah increment x = " + x);
+        
+        x--;  // decrement, x balik ke 5
+        System.out.println("Setelah decrement x = " + x);
+    }
+}
