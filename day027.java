618464a417e4f90f6b03367280831303b79a92ef
pratiwiindahkhofifah24-tech <pratiwiindahkhofifah24@gmail.com>
2025-10-04 22:11:03 +0800

    Create day027.java

---
+++ day027.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Codingday27 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+
+        System.out.printf("input : ");
+        int a = in.nextInt();       
+        System.out.printf("input : ");
+        int b = in.nextInt();
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+
+        System.out.println("Nilai variabel a setelah di tukar "+ a);
+        System.out.println("Nilai variabel b setelah di tukar "+ b);
+
+    }
+
+}
